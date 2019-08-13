d6665661991b81de4c43eb58c8eaceeff5733a49
Huang Jingpei <huangjingpei@gmail.com>
2019-08-13 08:01:39 +0800

    migrate the code from medooze

---
+++ config.mk
@@ -0,0 +1,13 @@
+#################################
+# Config file
+##################################
+LOG		  = yes
+DEBUG 		  = yes
+SANITIZE          = no
+STATIC		  = no
+VADWEBRTC	  = no
+IMAGEMAGICK       = no
+SRCDIR		  = .
+TARGET		  = ./out
+CEF		  = no
+CEF_DIR 	  = /usr/local/src/cef_3.2478.1316/
